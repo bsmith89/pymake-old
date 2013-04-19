da5f18b76202a0b8ee5c04d389f47d707e22069b
Byron Smith <bsmith89@gmail.com>
2013-04-19 13:24:23 -0400

    It's *ALIVE*

--- pymake.py
+++ pymake.py
@@ -1,7 +1,42 @@
+#! /usr/bin/env python
+"""A GNU Make replacement in python.
+
+Example:
+
+>>> rules = [Rule(trgt=r'all\.test\.txt',
+...               preqs=['this.test.txt', 'that.test.txt',
+...                      'theother.test.txt'],
+...               recipe='cat {all_preqs} > all.test.txt'),
+...          Rule(trgt=r'this\.test.txt',
+...               preqs='foo.thing',
+...               recipe='cat foo.thing > this.test.txt'),
+...          Rule(trgt=r'(.*)\.test\.txt',
+...               recipe='echo {1} > {trgt}'),
+...          Rule(trgt='clean', recipe='rm *.test.txt foo.thing')]
+>>> foo = open('foo.thing', 'w')
+>>> foo.write('booooyaaaaa!!!!')
+15
+>>> foo.close()
+>>> make(rules, 'all.test.txt')
+cat foo.thing > this.test.txt
+echo that > that.test.txt
+echo theother > theother.test.txt
+cat this.test.txt that.test.txt theother.test.txt > all.test.txt
+
+>>> make(rules, 'clean')
+rm *.test.txt foo.thing
+
+These rule sets and targets can be put into a file, pymake can
+be imported, and the final script serves as a complete makefile.
+
+"""
+
+
 import subprocess
 import re
 import os
 import shutil
+import datetime
 
 
 def clean_strings(strings):
@@ -17,17 +52,19 @@ def clean_strings(strings):
     return clean_strings
 
 class Rule():
-    def __init__(self, trgt_pat, preq_pats="", recipe_pat="", env={}):
+    def __init__(self, trgt, preqs="",
+                 recipe="", env={}):
         # Immediately replace all format identifiers in trgt and preqs.
         self.env = env
-        self.target_pattern = "^" + trgt_pat.format_map(env) + "$"
-        if isinstance(preq_pats, str):
-            self.prerequisite_patterns = [preq_pats.format_map(env)]
+        self.target_pattern = "^" + trgt.format_map(env) + "$"
+        if isinstance(preqs, str):
+            self.prerequisite_patterns = [preqs.format_map(env)]
         else:
             self.prerequisite_patterns = [pattern.format_map(env)
-                                          for pattern in preq_pats]
+                                          for pattern in preqs]
+        # TODO: Figure out how to make cleaning the pre-requisites unecessary
         self.prerequisite_patterns = clean_strings(self.prerequisite_patterns)
-        self.recipe_pattern = recipe_pat
+        self.recipe_pattern = recipe
 
     def __repr__(self):
         return ("Rule(trgt='{trgt}', "
@@ -76,12 +113,37 @@ class Rule():
     def make_task(self, trgt):
         # The trgt should always match the pattern.
         assert self.applies(trgt)
-        return Task(trgt, self._make_preqs(trgt), self._make_recipe(trgt))
+        return Task(trgt, self._make_preqs(trgt),
+                    self._make_recipe(trgt))
 
 
-class Task():
-    def __init__(self, trgt, preqs, recipe):
+class Requirement():
+    def __init__(self, trgt):
         self.target = trgt
+
+    def __repr__(self):
+        return "Requirement(trgt={self.target!r})".format(self=self)
+
+    def __str__(self):
+        return self.target
+
+    def last_update(self):
+        """Return the last time the requirement was updated.
+
+        The time returned determines the whether or not other tasks are
+        considered up to date, so if you want all tasks which depend on
+        the given task to run, this function should return a larger value.
+
+        """
+        if os.path.exists(self.target):
+            return os.path.getmtime(self.target)
+        else:
+            return 0.0
+
+
+class Task(Requirement):
+    def __init__(self, trgt, preqs, recipe):
+        super(Task, self).__init__(trgt=trgt)
         self.prerequisites = preqs
         self.recipe = recipe
 
@@ -99,8 +161,8 @@ class Task():
 
 
 def build_task_tree(rules, trgt):
-    """Build a task dependency tree by walking it recursively."""
-    rules = list(rules)  # Copy the rules list so that functions are indep.
+    """Build a dependency tree by walking it recursively."""
+    rules = list(rules)  # Copy the rules list so that we can edit in place.
     trgt = trgt.strip()
     if trgt is None:
         return None
@@ -109,12 +171,11 @@ def build_task_tree(rules, trgt):
     target_rule = None
     for i, rule in enumerate(rules):
         if rule.applies(trgt):
-            target_rule = rule
-            rules.pop(i)
+            target_rule = rules.pop(i)
             break
     if target_rule is None:
         if os.path.exists(trgt):
-            return trgt
+            return (Requirement(trgt),)
         else:
             raise ValueError(("No rule defined for {trgt!r}, or there is a "
                               "cycle in the dependency graph.")
@@ -123,24 +184,26 @@ def build_task_tree(rules, trgt):
     preqs = task.prerequisites
     preq_trees = [build_task_tree(rules, preq_trgt)
                   for preq_trgt in preqs]
-    branch = (task, preq_trees)
+    branch = (task,) + tuple(preq_trees)
     return branch
 
 def run_task_tree(tree):
-    task = tree[0]
-    preq_trees = tree[1]
+    requirement = tree[0]
+    last_tree_update = 0.0
+    preq_trees = tree[1:]
     for preq in preq_trees:
-        run_task_tree(preq)
-    task.run()
+        last_tree_update = max(run_task_tree(preq), last_tree_update)
+    if last_tree_update >= requirement.last_update():
+        requirement.run()
+        return datetime.datetime.now().timestamp()
+    else:
+        return max(last_tree_update, requirement.last_update())
 
-def make(trgt, rules):
-    tree = build_task_tree(trgt, rules)
-
-
+def make(rules, trgt):
+    tree = build_task_tree(rules, trgt)
+    run_task_tree(tree)
 
 
 if __name__ == "__main__":
-    rules = [Rule('all', ['this.txt', 'that.txt', 'theother.txt']),
-             Rule('(.*).txt', '', 'echo {1} > {trgt}')]
-    tree = build_task_tree(rules, 'all')
-    print(tree)
+    import doctest
+    doctest.testmod()
