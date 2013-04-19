575882c059d0ba51383d01984d802adb9e8ff0dd
Byron Smith <bsmith89@gmail.com>
2013-04-19 17:43:37 -0400

    Reorganized and updated setup script

--- setup.py
+++ setup.py
@@ -2,7 +2,10 @@ from distutils.core import setup
 
 
 
-
 setup(name='pymake',
       version='0.1.1',
-      py_modules=['pymake'])
+      url='https://github.com/bsmith89/pymake',
+      author='Byron J Smith',
+      author_email='bsmith89@gmail.com',
+      py_modules=['pymake'],
+      package_dir = {'': 'lib'})
