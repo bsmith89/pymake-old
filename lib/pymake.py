b32e137c8e6b7235bf70e045836583517c36dedb
Byron Smith <bsmith89@gmail.com>
2013-04-21 14:09:08 -0400

    Small refactor, made tasks for object oriented.

--- lib/pymake.py
+++ lib/pymake.py
@@ -4,10 +4,12 @@
 Example:
 
 >>> # Define a list of rules in order of priority:
->>> rules = [Rule(trgt=r'all\.test\.txt',
+>>> rules = [Rule(trgt=r'all{some_key}\.test\.txt',
 ...               preqs=['this.test.txt', 'that.test.txt',
 ...                      'theother.test.txt'],
-...               recipe='cat {all_preqs} > all.test.txt'),
+...               recipe=('sleep 1\\n'  # Simulates a longer running job.
+...                       'cat {all_preqs} > all{some_key}.test.txt'),
+...               env=dict(some_key=5)),
 ...          Rule(trgt=r'this\.test.txt',
 ...               preqs='foo.thing',
 ...               recipe='cat foo.thing > this.test.txt'),
@@ -19,28 +21,32 @@ Example:
 >>> foo.write('booooyaaaaa!!!!\\n')
 16
 >>> foo.close()
+>>> import time
+>>> time.sleep(1)  # Because otherwise the timestamps are too similar.
 >>> # And now call make().  It really is that easy!
->>> make(rules, 'all.test.txt')
+>>> make('all5.test.txt', rules)
 cat foo.thing > this.test.txt
 echo that > that.test.txt
 echo theother > theother.test.txt
-cat this.test.txt that.test.txt theother.test.txt > all.test.txt
+sleep 1
+cat this.test.txt that.test.txt theother.test.txt > all5.test.txt
 
 >>> # And the contents:
->>> open('all.test.txt').read()
+>>> open('all5.test.txt').read()
 'booooyaaaaa!!!!\\nthat\\ntheother\\n'
->>> ## Now what happens if we try to make it again?
->>> # make(rules, 'all.test.txt')  # TODO: Why isn't this doctest working?
->>> ## Everythings up to date alread!
+>>> # Now what happens if we try to make it again?
+>>> make('all5.test.txt', rules)
+>>> # Everythings up to date alread!
 >>> # If we touch foo.thing...
 >>> os.utime('foo.thing')
 >>> # One branch of the tree must be re-run.
->>> make(rules, 'all.test.txt')
+>>> make('all5.test.txt', rules)
 cat foo.thing > this.test.txt
-cat this.test.txt that.test.txt theother.test.txt > all.test.txt
+sleep 1
+cat this.test.txt that.test.txt theother.test.txt > all5.test.txt
 
 >>> # Oh look, we defined a 'clean' rule!
->>> make(rules, 'clean')
+>>> make('clean', rules)
 rm *.test.txt foo.thing
 
 
@@ -175,15 +181,13 @@ class Rule():
 class Requirement():
     """Base class for all requirements.
 
-    This class is also used for file requirements.
-
     """
 
     def __init__(self, trgt):
         self.target = trgt
 
     def __repr__(self):
-        return "Requirement(trgt={self.target!r})".format(self=self)
+        return "{self.__class__}(trgt={self.target!r})".format(self=self)
 
     def __str__(self):
         return self.target
@@ -196,6 +200,18 @@ class Requirement():
         the given task to run, this function should return a larger value.
 
         """
+        raise NotImplementedError("The base Requirement class has not "
+                                  "implemented last_update, but it *should* "
+                                  "be implemented in all functioning "
+                                  "sub-classes")
+
+
+class FileReq(Requirement):
+    """A Requirement subclass used for file requirements."""
+    def __init__(self, trgt_path):
+        super(FileReq, self).__init__(trgt = trgt_path)
+
+    def last_update(self):
         if os.path.exists(self.target):
             return os.path.getmtime(self.target)
         else:
@@ -218,13 +234,30 @@ class Task(Requirement):
         # TODO
         return self.recipe
 
+    def last_update(self):
+        if os.path.exists(self.target):
+            return os.path.getmtime(self.target)
+        elif self.prerequisites in (None, [], "", [""]):
+            return 0.0
+        else:
+            return 0.0
+
     def run(self):
         """Run the task to create the target."""
-        print(self.recipe)
+        print(self.recipe)  # TODO: make the printout different color.
         subprocess.check_call(self.recipe, shell=True)
 
 
-def build_task_tree(rules, trgt):
+class DepTree():
+    """A dependency tree which is defined recursively."""
+    def __init__(target, preqs=[]):
+        # TODO
+        raise NotImplementedError("The dependency tree data structure has "
+                                  "not yet been implemented.")
+
+
+
+def build_task_tree(trgt, rules):
     """Build a dependency tree by walking a rules recursively."""
     rules = list(rules)  # Copy the rules list so that we can edit in place.
     trgt = trgt.strip()
@@ -239,14 +272,14 @@ def build_task_tree(rules, trgt):
             break
     if target_rule is None:
         if os.path.exists(trgt):
-            return (Requirement(trgt),)
+            return (FileReq(trgt),)
         else:
             raise ValueError(("No rule defined for {trgt!r}, or there is a "
                               "cycle in the dependency graph.")
                              .format(trgt=trgt))
     task = target_rule.make_task(trgt)
     preqs = task.prerequisites
-    preq_trees = [build_task_tree(rules, preq_trgt)
+    preq_trees = [build_task_tree(preq_trgt, rules)
                   for preq_trgt in preqs]
     branch = (task,) + tuple(preq_trees)
     return branch
@@ -270,7 +303,7 @@ def run_task_tree(tree):
 def pll_run_task_tree(tree):
     """Run a dependency tree by walking it recursively.
 
-    Parallelize running by making multiple threads for each
+    Parallelize running by making a new thread for each
     pre-requisite of a task.
 
     """
@@ -289,9 +322,9 @@ def pll_run_task_tree(tree):
 
 
 
-def make(rules, trgt, parallel=False):
+def make(trgt, rules, parallel=False):
     """Construct the task tree and run it."""
-    tree = build_task_tree(rules, trgt)
+    tree = build_task_tree(trgt, rules)
     if parallel:
         pll_run_task_tree(tree)
     else:
@@ -299,5 +332,26 @@ def make(rules, trgt, parallel=False):
 
 
 if __name__ == "__main__":
-    import doctest
-    doctest.testmod()
+    rules = [Rule(trgt=r'all{some_key}\.test\.txt',
+                  preqs=['this.test.txt', 'that.test.txt',
+                         'theother.test.txt'],
+                  recipe=('cat {all_preqs} > {trgt}'),
+                  env=dict(some_key=5)),
+             Rule(trgt=r'this\.test.txt',
+                  preqs='foo.thing',
+                  recipe='cat foo.thing > this.test.txt'),
+             Rule(trgt=r'(.*)\.test\.txt',
+                  recipe='echo {1} > {trgt}'),
+             Rule(trgt='clean', recipe='rm *.test.txt foo.thing')]
+    foo = open('foo.thing', 'w')
+    foo.write('booooyaaaaa!!!!\\n')
+    foo.close()
+    make('all5.test.txt', rules)
+    print(open('all5.test.txt').read())
+    make('all5.test.txt', rules)
+    os.utime('foo.thing')
+    # One branch of the tree must be re-run.
+    make('all5.test.txt', rules)
+    make('clean', rules)
+    #import doctest
+    #doctest.testmod()
