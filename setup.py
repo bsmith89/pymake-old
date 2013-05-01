21911f57adc2ff2b68c6e4efe1c8dc9fa4287c00
Byron Smith <bsmith89@gmail.com>
2013-05-01 19:44:24 -0400

    Updated the requirements and incremented the patch number.

--- setup.py
+++ setup.py
@@ -3,10 +3,12 @@ from distutils.core import setup
 
 
 setup(name='pymake',
-      version='0.1.3',
+      version='0.1.4',
       url='https://github.com/bsmith89/pymake',
       author='Byron J Smith',
       author_email='bsmith89@gmail.com',
       py_modules=['pymake'],
       package_dir = {'': 'lib'},
-      requires=["termcolor"])
+      install_requires=["termcolor"],
+      extras_require={"visualize": ["pydot >= 1.0.15"]},
+      dependency_links = ["hg+https://bitbucket.org/prologic/pydot"])
