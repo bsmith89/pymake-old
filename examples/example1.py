f1f51ba4c33bc4acdbe18ddd92c69d067d554015
Byron Smith <bsmith89@gmail.com>
2013-05-15 13:17:57 -0400

    Added an example which takes advantage of the order-only requirements

---
+++ examples/example1.py
@@ -0,0 +1,34 @@
+import sys
+from pymake import Rule, visualize_graph, make
+from time import sleep
+
+EXT = "test"
+primary_req_path = "example1.txt"
+
+rules = [Rule("all", preqs=["branch1", "branch2"]),
+         Rule("branch([0-9])",
+              preqs=["branch{1}/final1.{EXT}",
+                     "branch{1}/final2.{EXT}"],
+              EXT=EXT),
+         Rule("branch([0-9])/final([0-9]).{EXT}",
+              preqs=["branch{1}/pre{2}1.{EXT}",
+                     "branch{1}/pre{2}2.{EXT}"],
+              recipe="cat {all_preqs} > {trgt}; sleep 1",
+              EXT=EXT),
+         Rule("branch([0-9])/pre([0-9])([0-9]).{EXT}",
+              preqs=[primary_req_path, "branch{1}/"],
+              recipe=("echo {preqs[0]} branch{1} pre{2} other{3} | \\\n"
+                      "cat - {preqs[0]} > {trgt} \n"
+                      "sleep 1"),
+              EXT=EXT),
+         Rule(".*/", recipe="mkdir {trgt} \nsleep 1", order_only=True),
+         Rule("clean", recipe="rm -r branch*/ example1.png")]
+
+if __name__ == '__main__':
+    try:
+        target = sys.argv[1]
+    except IndexError:
+        target = "all"
+    visualize_graph(target, rules)
+    make(target, rules, parallel=True)
+
