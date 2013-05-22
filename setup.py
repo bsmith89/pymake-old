72f21418a0d5d63242d08b730cad29e5c00095ea
Byron Smith <bsmith89@gmail.com>
2013-05-22 11:07:24 -0400

    Incremented the patch number.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 
 setup(name='pymake',
-      version='0.1.5',
+      version='0.1.6',
       url='https://github.com/bsmith89/pymake',
       author='Byron J Smith',
       author_email='bsmith89@gmail.com',
